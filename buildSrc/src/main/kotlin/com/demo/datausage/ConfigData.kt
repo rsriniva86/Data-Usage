397a4f5b86519920d8cc115ddf1be91e99fe1efe
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-04 18:08:48 +0800

    Feature : BuildSrc : Introduce buildSrc module

---
+++ buildSrc/src/main/kotlin/com/demo/datausage/ConfigData.kt
@@ -0,0 +1,13 @@
+package com.demo.datausage
+
+object ConfigData {
+
+    const val compileSdkVersion = 33
+    const val minSdkVersion = 28
+    const val targetSdkVersion = 33
+    const val applicationId = "com.demo.datausage"
+    const val kotlinCompilerExtensionVersion = Versions.kotlin
+    const val applicationVersionCode = 1
+    const val applicationVersionName = "1.0"
+
+}
