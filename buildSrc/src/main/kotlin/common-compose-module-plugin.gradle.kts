e8e23a6341d658d8fa3f07b43185beeec4055d3a
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-05 21:45:49 +0800

    Feature : Theme : Create and Use custom plugin from buildSrc

---
+++ buildSrc/src/main/kotlin/common-compose-module-plugin.gradle.kts
@@ -0,0 +1,31 @@
+import com.demo.datausage.build.ConfigData
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = ConfigData.compileSdkVersion
+
+    defaultConfig {
+        minSdk = ConfigData.minSdkVersion
+        targetSdk = ConfigData.targetSdkVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = ConfigData.kotlinCompilerExtensionVersion
+    }
+
+}
