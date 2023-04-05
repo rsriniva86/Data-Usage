53c974e1e109b5e962fadb4db02bbdff4816a523
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-05 15:54:10 +0800

    Features : Setup Modules : Add modules to settings gradle file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "Data Usage"
 include (":app")
 include(":core")
 include(":core:repository")
+include(":features")
+include(":features:consumption")
+include(":features:consumption:years")
+include(":features:consumption:qtr")
