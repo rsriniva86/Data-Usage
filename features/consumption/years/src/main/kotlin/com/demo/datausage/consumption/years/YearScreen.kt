dcae76e4ac95a97cd9804e49dbeee7834d207695
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-06 23:24:57 +0800

    Feature : year screen : update to use card

--- features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
+++ features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,16 +41,23 @@ fun YearScreen() {
 
 @Composable
 private fun YearItem(dataItem: YearWiseData) {
-    Column (modifier = Modifier.padding(all = 8.dp)){
-        Row {
-            Text(text = "Year : ")
-            Text (text= "${dataItem.year}")
-        }
-        Row {
-            Text(text = "Usage : ")
-            Text (text= "${dataItem.value}")
+    Card(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+    ) {
+        Column (modifier =Modifier.padding(all =8.dp)){
+            Row {
+                Text(text = "Year : ")
+                Text (text= "${dataItem.year}")
+            }
+            Row {
+                Text(text = "Usage : ")
+                Text (text= "${dataItem.value}")
+            }
         }
     }
+
 
 }
 
