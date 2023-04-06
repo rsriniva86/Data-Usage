7c0797763ce9fcdf43e6d9a4a2c5a393fa54bd28
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-06 23:12:35 +0800

    Feature : Year Screen : Update year Screen

--- features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
+++ features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
@@ -3,12 +3,15 @@ package com.demo.datausage.consumption.years
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.demo.datausage.domainmodels.Datatype
 import com.demo.datausage.domainmodels.YearWiseData
 
@@ -17,7 +20,14 @@ fun YearScreen() {
     val data = provideDummyData().toList()
 
     Column{
-        Text(text = "Year Screen")
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+            ,
+            textAlign = TextAlign.Center,
+            text = "Year Screen",
+
+        )
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
             items(data){
                 YearItem(dataItem = it)
@@ -30,7 +40,7 @@ fun YearScreen() {
 
 @Composable
 private fun YearItem(dataItem: YearWiseData) {
-    Column {
+    Column (modifier = Modifier.padding(all = 8.dp)){
         Row {
             Text(text = "Year : ")
             Text (text= "${dataItem.year}")
@@ -46,7 +56,7 @@ private fun YearItem(dataItem: YearWiseData) {
 fun provideDummyData(): List<YearWiseData> {
 
     val startYear = 2000
-    val startValue = 101
+    val startValue = 100
 
     val list = mutableListOf<YearWiseData>()
 
