25ea77cd712ae1c1f6496577c5b096f509bfa4cb
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-08 14:13:40 +0800

    Feature : Logging : Added Logging module

---
+++ common/logging/build.gradle.kts
@@ -0,0 +1,16 @@
+import com.demo.datausage.build.addAndroidDependencies
+import com.demo.datausage.build.addDependencyInjectionDependencies
+import com.demo.datausage.build.addRetrofitDependencies
+
+plugins {
+    id("common-kotlin-module-plugin")
+}
+
+android {
+    namespace = "com.demo.datausage.common.logging"
+}
+
+dependencies {
+    addAndroidDependencies()
+}
+
