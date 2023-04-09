12c788d81e802abe4e1076ed4e2d89aed1feeafd
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-09 08:56:29 +0800

    Feature : UI Improvement : Add scaffold to year screen

--- features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
+++ features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
@@ -1,5 +1,7 @@
 package com.demo.datausage.consumption.years
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,7 +10,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -18,35 +22,44 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.demo.datausage.domainmodels.DataUsageScreens
-import com.demo.datausage.domainmodels.Datatype
 import com.demo.datausage.domainmodels.YearWiseData
 import org.koin.androidx.compose.getViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun YearScreen(
-    navController: NavController,
-    yearScreenViewModel: YearScreenViewModel
+    navController: NavController, yearScreenViewModel: YearScreenViewModel
 ) {
     val data = yearScreenViewModel.list.toList()
-    LaunchedEffect(Unit){
+    LaunchedEffect(Unit) {
         yearScreenViewModel.getYearData()
     }
 
-    Column{
-        Text(
-            modifier = Modifier
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            text = "Year Screen",
-        )
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Year Screen",
+
+                    )
+            }
+        })
+    }) {
+
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            items(data){
+            items(data) {
                 YearItem(
-                    navController = navController,
-                    dataItem = it
+                    navController = navController, dataItem = it
                 )
             }
         }
+
     }
 
 
@@ -55,25 +68,21 @@ fun YearScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun YearItem(
-    dataItem: YearWiseData,
-    navController:NavController
+    dataItem: YearWiseData, navController: NavController
 ) {
-    Card(
-        modifier = Modifier
-            .padding(all = 8.dp)
-            .fillMaxWidth(),
-        onClick = {
-            navController.navigate(DataUsageScreens.QtrScreen.route+ "/${dataItem.year}")
-        }
-    ) {
-        Column (modifier =Modifier.padding(all =8.dp)){
+    Card(modifier = Modifier
+        .padding(all = 8.dp)
+        .fillMaxWidth(), onClick = {
+        navController.navigate(DataUsageScreens.QtrScreen.route + "/${dataItem.year}")
+    }) {
+        Column(modifier = Modifier.padding(all = 8.dp)) {
             Row {
                 Text(text = "Year : ")
-                Text (text= "${dataItem.year}")
+                Text(text = "${dataItem.year}")
             }
             Row {
                 Text(text = "Usage : ")
-                Text (text= "${dataItem.value}")
+                Text(text = "${dataItem.value}")
             }
         }
     }
@@ -86,7 +95,6 @@ private fun YearItem(
 private fun PreviewYearScreen() {
     val navController = rememberNavController()
     YearScreen(
-        navController = navController,
-        yearScreenViewModel = getViewModel<YearScreenViewModel>()
+        navController = navController, yearScreenViewModel = getViewModel<YearScreenViewModel>()
     )
 }
