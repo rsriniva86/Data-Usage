f307267bb25e19432f097b2ee3bd959c9b96e645
rsriniva <54564955+rsriniva86@users.noreply.github.com>
2023-04-09 09:16:13 +0800

    Feature : UI Improvements : manage text in Year screen

--- features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
+++ features/consumption/years/src/main/kotlin/com/demo/datausage/consumption/years/YearScreen.kt
@@ -1,6 +1,7 @@
 package com.demo.datausage.consumption.years
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,11 +11,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +42,7 @@ fun YearScreen(
     Scaffold(topBar = {
         TopAppBar(title = {
             Row(
+
                 horizontalArrangement = Arrangement.Start,
                 modifier = Modifier.padding(start = 8.dp)
             ) {
@@ -46,13 +50,18 @@ fun YearScreen(
                     modifier = Modifier.fillMaxWidth(),
                     textAlign = TextAlign.Center,
                     text = "Year Screen",
-
+                    style = MaterialTheme.typography.headlineLarge
                     )
             }
         })
     }) {
 
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        LazyColumn(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.background)
+                .fillMaxWidth()
+                .padding(top = 60.dp)
+        ) {
             items(data) {
                 YearItem(
                     navController = navController, dataItem = it
@@ -70,19 +79,52 @@ fun YearScreen(
 private fun YearItem(
     dataItem: YearWiseData, navController: NavController
 ) {
-    Card(modifier = Modifier
-        .padding(all = 8.dp)
-        .fillMaxWidth(), onClick = {
+    Card(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth(),
+        onClick = {
         navController.navigate(DataUsageScreens.QtrScreen.route + "/${dataItem.year}")
     }) {
-        Column(modifier = Modifier.padding(all = 8.dp)) {
-            Row {
-                Text(text = "Year : ")
-                Text(text = "${dataItem.year}")
+        Column(
+            modifier =
+            Modifier
+                .background(MaterialTheme.colorScheme.primary)
+                .fillMaxWidth()
+                .padding(all = 8.dp)
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "Year : ",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center
+
+                )
+                Text(
+                    text = "${dataItem.year}",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
+                )
             }
-            Row {
-                Text(text = "Usage : ")
-                Text(text = "${dataItem.value}")
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "Usage : ",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "${dataItem.value}",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
